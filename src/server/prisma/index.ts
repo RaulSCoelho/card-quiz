6812078387d4a7820e927de5fb77d07a902c3023
RaulSCoelho <raul.s.coelho@gmail.com>
2023-08-28 02:20:59 -0300

    prisma

--- src/server/prisma/index.ts
+++ src/server/prisma/index.ts
@@ -1,18 +1,3 @@
 import { PrismaClient } from '@prisma/client'
 
-let client: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  client = new PrismaClient()
-  console.log('production')
-} else {
-  if (!(global as any).prisma) {
-    ;(global as any).prisma = new PrismaClient()
-    console.log('aqui stoy not production gerando novo client')
-  }
-
-  client = (global as any).prisma
-  console.log('aqui stoy not production')
-}
-
-export const prisma = client
+export const prisma = new PrismaClient()
