ce81e1dd7178f9d8b571250a9386fe7c5bd4ac64
RaulSCoelho <raul.s.coelho@gmail.com>
2023-08-28 02:27:55 -0300

    prisma

--- src/server/prisma/index.ts
+++ src/server/prisma/index.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+declare global {
+  var prisma: PrismaClient
+}
+
+let client: PrismaClient
+if (process.env.NODE_ENV === 'production') {
+  client = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  client = global.prisma
+}
+
+export const prisma = client
