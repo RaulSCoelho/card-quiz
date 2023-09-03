affa0f7547bf39b8e0096bb2898c9ab1f345e33a
RaulSCoelho <raul.s.coelho@gmail.com>
2023-09-03 15:33:40 -0300

    localstorage match

--- src/hooks/useMatch.ts
+++ src/hooks/useMatch.ts
@@ -14,30 +14,55 @@ interface MatchState extends Match {
   started: boolean
 }
 
-export const useMatch = create<MatchState>(set => ({
+const localStorageKey = 'match'
+const savedState = localStorage.getItem(localStorageKey)
+
+const initialState = {
   id: uuid(),
   players: [],
-  started: false,
+  started: false
+}
+
+export const useMatch = create<MatchState>(set => ({
+  ...initialState,
+  // If there's a saved state in localStorage, use it; otherwise, use the initial state
+  ...(savedState ? JSON.parse(savedState) : {}),
   addPlayer: (player: string) =>
-    set(({ players }) => {
+    set(match => {
       if (!player) {
         return { error: 'Por favor escolha um nome' }
       }
       if (player.length > 17) {
         return { error: 'Por favor escolha um nome com até 16 letras' }
       }
-      if (players.some(p => p.username === player)) {
+      if (match.players.some(p => p.username === player)) {
         return { error: 'Jogador com esse nome já foi cadastrado, por favor escolha outro nome' }
       }
 
-      return { players: [...players, { id: uuid(), username: player, score: 0 }], error: undefined }
+      const updatedPlayers = [...match.players, { id: uuid(), username: player, score: 0 }]
+      localStorage.setItem(localStorageKey, JSON.stringify({ ...match, players: updatedPlayers }))
+
+      return { players: updatedPlayers, error: undefined }
     }),
-  removePlayer: (player: string) => set(({ players }) => ({ players: players.filter(p => p.username !== player) })),
+  removePlayer: (player: string) =>
+    set(match => {
+      const updatedPlayers = match.players.filter(p => p.username !== player)
+      localStorage.setItem(localStorageKey, JSON.stringify({ ...match, players: updatedPlayers }))
+      return { players: updatedPlayers }
+    }),
   clearError: () => set(() => ({ error: undefined })),
   start: () =>
-    set(({ players }) => {
-      if (players.length > 0) return { started: true }
-      return { started: false, error: 'Adicione um jogador para poder iniciar' }
+    set(match => {
+      if (match.players.length > 0) {
+        const updatedMatch = { ...match, started: true }
+        localStorage.setItem(localStorageKey, JSON.stringify(updatedMatch))
+        return updatedMatch
+      }
+      return { ...match, started: false, error: 'Adicione um jogador para poder iniciar' }
     }),
-  end: () => set(() => ({ players: [], started: false }))
+  end: () =>
+    set(() => {
+      localStorage.removeItem(localStorageKey)
+      return { ...initialState }
+    })
 }))
